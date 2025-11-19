7ed8a10cdd58c07d353742bfbf23f67fcc655642
MohammadIndraA <akunpertamasaya5@gmail.com>
2025-11-19 10:18:06 +0700

    change query

--- app/Http/Controllers/Api/BlokController.php
+++ app/Http/Controllers/Api/BlokController.php
@@ -12,10 +12,12 @@ class BlokController extends Controller
         return response()->json([
             'message' => 'Data Berhasil Diambil',
             'data' => Blok::with(['kaplings' => function($query) {
-                        $query->orderBy('nama', 'asc'); // urutkan kaplings ASC
-                        }])
-                        ->orderBy('nama', 'asc') // urutkan blok ASC
-                        ->get(),
+                        $query->orderByRaw("LEFT(kode, 1) ASC");
+                        $query->orderByRaw("CAST(SUBSTRING(kode, 2) AS UNSIGNED) ASC");
+                    }])
+                    ->orderByRaw("LEFT(kode, 1) ASC")
+                    ->orderByRaw("CAST(SUBSTRING(kode, 2) AS UNSIGNED) ASC")
+                    ->get(),
             'status' => 'success',
         ]);
     }
