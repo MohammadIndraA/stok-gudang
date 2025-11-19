ce0ecffdf9078e789e4816324aa934b416d800a9
MohammadIndraA <akunpertamasaya5@gmail.com>
2025-11-19 10:23:26 +0700

    change query  by nama

--- app/Http/Controllers/Api/BlokController.php
+++ app/Http/Controllers/Api/BlokController.php
@@ -12,11 +12,11 @@ class BlokController extends Controller
         return response()->json([
             'message' => 'Data Berhasil Diambil',
             'data' => Blok::with(['kaplings' => function($query) {
-                        $query->orderByRaw("LEFT(kode, 1) ASC");
-                        $query->orderByRaw("CAST(SUBSTRING(kode, 2) AS UNSIGNED) ASC");
+                        $query->orderByRaw("LEFT(nama, 1) ASC");
+                        $query->orderByRaw("CAST(SUBSTRING(nama, 2) AS UNSIGNED) ASC");
                     }])
-                    ->orderByRaw("LEFT(kode, 1) ASC")
-                    ->orderByRaw("CAST(SUBSTRING(kode, 2) AS UNSIGNED) ASC")
+                    ->orderByRaw("LEFT(nama, 1) ASC")
+                    ->orderByRaw("CAST(SUBSTRING(nama, 2) AS UNSIGNED) ASC")
                     ->get(),
             'status' => 'success',
         ]);
