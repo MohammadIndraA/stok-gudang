e51f74c61aefaf89c4348742ee6f4552f1645b5b
sicipatujah-code <sicipatujah@gmail.com>
2025-11-30 13:12:11 +0700

    ubah search component v.

--- resources/views/components/select-search.blade.php
+++ resources/views/components/select-search.blade.php
@@ -4,7 +4,14 @@
     </label>
 
     <select name="{{ $name }}" id="{{ $name }}"
-        {{ $attributes->merge(['class' => 'form-input w-full rounded-md mt-1 border border-slate-300/60 dark:border-slate-700 dark:text-slate-300 bg-transparent px-3 py-1 focus:outline-none focus:ring-0 placeholder:text-slate-400/70 placeholder:font-normal placeholder:text-sm hover:border-slate-400 focus:border-primary-500 dark:focus:border-primary-500 dark:hover:border-slate-700']) }}>
+        {{ $attributes->merge([
+            'class' => 'form-input w-full rounded-md mt-1 border border-slate-300/60 
+                                dark:border-slate-700 dark:text-slate-300 bg-transparent px-3 py-1 
+                                focus:outline-none focus:ring-0 placeholder:text-slate-400/70 
+                                placeholder:font-normal placeholder:text-sm hover:border-slate-400 
+                                focus:border-primary-500 dark:focus:border-primary-500 
+                                dark:hover:border-slate-700',
+        ]) }}>
 
         @if ($placeholder)
             <option value="">{{ $placeholder }}</option>
